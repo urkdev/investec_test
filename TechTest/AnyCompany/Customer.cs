6b2fc7155121735186956aeba81a4590cd0db1ce
urkdev <uzam.r.khan@gmail.com>
2018-11-29 04:14:52 +0000

    finished test

---
+++ TechTest/AnyCompany/Customer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AnyCompany
+{
+    public class Customer
+    {
+        public string Country { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+
+        public string Name { get; set; }
+
+        public int CustomerId { get; set; }
+
+        public IEnumerable<Order> CustomerOrders { get; set; }
+    }
+}
